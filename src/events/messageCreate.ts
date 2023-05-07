ab0c31c14e8322b010b2e8110afd6febafdad0f3
Emitter <xperimentschaftler@gmail.com>
2023-05-07 18:23:01 +0600

    .

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -6,6 +6,7 @@ export const messageCreate_listener = (client: Client) => {
     client.on('messageCreate', async msg => {
         try{
             if(!msg.content.toLocaleLowerCase().startsWith("!happiness-tracker")) return;
+            if(!msg.member?.permissions.has('ModerateMembers')) return;
             await happiness_tracker(msg.channelId);
         }catch(err){
             console.log("Err on /events/messageCreate.ts");
